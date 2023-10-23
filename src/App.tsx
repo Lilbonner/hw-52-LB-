4f585688d3d229333da72a77b12b243f1236ff20
Daniyar Nasirov <lilbonnie666@gmail.com>
2023-10-23 14:03:37 +0600

    add lib dir & file cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import {useState} from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CardDeck from './lib/cardDeck';
+import './cards.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App: React.FC = () => {
+  const deck = new CardDeck();
+
+  const [currentCards, setCurrentCards] = useState(deck.getCards(4));
+
+  const handleGenerateCards = () => {
+    const newCards = deck.getCards(4);
+
+    setCurrentCards(newCards);
+  };
 
   return (
-    <>
+    <div>
+      <h1>Poker</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo"/>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo"/>
-        </a>
+        {currentCards.map((card, index) => (
+          <p key={index}>{`${card.rank} ${card.suit}`}</p>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <button onClick={handleGenerateCards}>new hand</button>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/lib/cardDeck.ts
@@ -0,0 +1,43 @@
+import Card from './card';
+
+class CardDeck {
+  private cards: Card[] = [];
+
+  constructor() {
+    const suits = ['♥', '♦', '♠', '♣'];
+    const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+    for (const suit of suits) {
+      for (const rank of ranks) {
+        const card = new Card(rank, suit);
+        this.cards.push(card);
+      }
+    }
+  }
+
+  getCard(): Card {
+    if (this.cards.length === 0) {
+      throw new Error('Колода пуста');
+    }
+
+    const randomIndex = Math.floor(Math.random() * this.cards.length);
+    return this.cards.splice(randomIndex, 1)[0];
+  }
+
+  getCards(howMany: number): Card[] {
+    const drawnCards: Card[] = [];
+
+    for (let i = 0; i < howMany; i++) {
+      if (this.cards.length === 0) {
+        break;
+      }
+
+      const randomIndex = Math.floor(Math.random() * this.cards.length);
+      drawnCards.push(this.cards.splice(randomIndex, 1)[0]);
+    }
+
+    return drawnCards;
+  }
+}
+
+export default CardDeck;
